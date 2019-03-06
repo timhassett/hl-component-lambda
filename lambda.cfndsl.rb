50a6521d60338142e6327f04dabe72415b237f01
Tim Hassett <t.hassett@base2services.com>
2019-03-06 13:56:24 +1100

    removed ref

--- lambda.cfndsl.rb
+++ lambda.cfndsl.rb
@@ -72,7 +72,7 @@ CloudFormation do
     end
 
     Output("#{function_name}Arn") {
-        Value(Ref(FnGetAtt(function_name, 'Arn')))
+        Value(FnGetAtt(function_name, 'Arn'))
       }
 
     Logs_LogGroup("#{function_name}LogGroup") do
